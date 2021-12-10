dbaef687b72497bb70acc1ad0898facb0c441e32
Aaron Deas <deas.aaron@gmail.com>
2021-12-10 12:20:28 -0500

    Added error handling (try, catch) to api routes so errors don't break API

--- src/controllers/homeRoutes.ts
+++ src/controllers/homeRoutes.ts
@@ -7,29 +7,42 @@ router.get('/', (req: Request, res: Response) => {
 });
 // -------- Will List ALL forms --------
 router.get('/forms', async (req: Request, res: Response) => {
-  const formController = new FormFunctions();
-  const { data } = await formController.getAllForms();
-  res.json({ data });
+  try {
+    const formController = new FormFunctions();
+    const { data } = await formController.getAllForms();
+    res.json({ data });
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // -------- Will List ALL Submissions for a given form --------
 // ie: /submissions/4539136/
 
 router.get('/submissions/:id', async (req: Request, res: Response) => {
-  const formController = new FormFunctions();
-  const { data } = await formController.getFormSubmissions(
-    parseInt(req.params.id)
-  );
-  res.json({ data });
+  try {
+    const formController = new FormFunctions();
+    const { data } = await formController.getFormSubmissions(
+      parseInt(req.params.id)
+    );
+    res.json({ data });
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
 });
+
 module.exports = router;
 
 // -------- Will List info for specific submission --------
 // ie: /submission/866922837/
 router.get('/submission/:id', async (req: Request, res: Response) => {
-  const formController = new FormFunctions();
-  const { data } = await formController.getFormSubmission(
-    parseInt(req.params.id)
-  );
-  res.json({ data });
+  try {
+    const formController = new FormFunctions();
+    const { data } = await formController.getFormSubmission(
+      parseInt(req.params.id)
+    );
+    res.json({ data });
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
 });
