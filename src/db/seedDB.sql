bf815cfbd88722b1de2197e03b5aa1527710f6c1
Aaron Deas <deas.aaron@gmail.com>
2021-12-10 14:38:31 -0500

    Changed DB field status so that the required fields on the form are set appropriately in the DB

--- src/db/seedDB.sql
+++ src/db/seedDB.sql
@@ -6,16 +6,59 @@ USE newtaskDB;
 
 CREATE TABLE newtaskform (
     id INT NOT NULL AUTO_INCREMENT,
-    requestor VARCHAR(255) NOT NULL,
+    requestor VARCHAR(255),
     client VARCHAR(255) NOT NULL,
     project VARCHAR(255) NOT NULL,
     task_titles VARCHAR(255) NOT NULL,
     priority INT NOT NULL DEFAULT 1,
     deadline DATE NOT NULL,
     lead_name VARCHAR(255) NOT NULL,
-    resource VARCHAR(255) NOT NULL,
+    resource VARCHAR(255),
     risk VARCHAR(255) NOT NULL DEFAULT 'Low',
-    task_descriptions VARCHAR(255) NOT NULL,
+    task_descriptions VARCHAR(255),
     file_count INT NOT NULL DEFAULT 1,
     PRIMARY KEY (id)
 );
+
+INSERT INTO
+    newtaskform (
+        requestor,
+        client,
+        project,
+        task_titles,
+        priority,
+        deadline,
+        lead_name,
+        resource,
+        risk,
+        task_descriptions,
+        file_count
+    )
+VALUES
+    (
+        'John Doe',
+        'Client A',
+        'Project A',
+        'Task A',
+        1,
+        '2020-01-01',
+        'John Doe',
+        'John Doe',
+        'Low',
+        'Task A',
+        1
+    ),
+    (
+        '',
+        'Client B',
+        'Project B',
+        'Do Stuff',
+        2,
+        '2020-01-01',
+        'John Doe',
+        '',
+        'Medium',
+        '',
+        1
+    ),
+)
