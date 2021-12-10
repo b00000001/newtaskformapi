f216b35a6d60f9fe937ef2c7fc9b3e2222db2746
Aaron Deas <deas.aaron@gmail.com>
2021-12-10 01:33:15 -0500

    Cleaned up code in FormFunctions class

--- src/formfunctions/index.ts
+++ src/formfunctions/index.ts
@@ -2,63 +2,45 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 export default class FormFunctions {
-  //-------- Get list of all Forms --------
-  getAllForms() {
-    const options = {
+  options: any;
+  constructor() {
+    this.options = {
       method: 'GET',
       headers: {
         Accept: 'application/json',
         Authorization: `Bearer ${process.env.BEARER_TOKEN}`
       }
     };
+  }
+  //-------- Get list of all Forms --------
+  getAllForms() {
     const data = axios.get(
       'https://www.formstack.com/api/v2/form.json?folders=false',
-      options
+      this.options
     );
     return data;
   }
   //-------- Get form by ID --------
   getForm(id: Number) {
-    const options = {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${process.env.BEARER_TOKEN}`
-      }
-    };
     const data = axios.get(
       `https://www.formstack.com/api/v2/form/${id}.json`,
-      options
+      this.options
     );
     return data;
   }
   // -------- Get form submissions --------
   getFormSubmissions(id: Number) {
-    const options = {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${process.env.BEARER_TOKEN}`
-      }
-    };
     const data = axios.get(
       `https://www.formstack.com/api/v2/form/${id}/submission.json`,
-      options
+      this.options
     );
     return data;
   }
   // -------- Get form submission by ID --------
   getFormSubmission(submissionID: Number) {
-    const options = {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${process.env.BEARER_TOKEN}`
-      }
-    };
     const data = axios.get(
       `https://www.formstack.com/api/v2/submission/${submissionID}.json`,
-      options
+      this.options
     );
     return data;
   }
